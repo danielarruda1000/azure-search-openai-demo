3250fc23af168527062af677834f27630343004f
Daniel Arruda <46331337+danielarruda1000@users.noreply.github.com>
2023-06-27 13:05:48 +0000

    Pending changes exported from your codespace

--- app/frontend/src/api/models.ts
+++ app/frontend/src/api/models.ts
@@ -1,7 +1,8 @@
 export const enum Approaches {
     RetrieveThenRead = "rtr",
     ReadRetrieveRead = "rrr",
-    ReadDecomposeAsk = "rda"
+    ReadDecomposeAsk = "rda",
+    ReadRetrieveReadFAISS = "rrf"
 }
 
 export type AskRequestOverrides = {
--- app/frontend/src/components/Example/ExampleList.tsx
+++ app/frontend/src/components/Example/ExampleList.tsx
@@ -9,11 +9,11 @@ export type ExampleModel = {
 
 const EXAMPLES: ExampleModel[] = [
     {
-        text: "What is included in my Northwind Health Plus plan that is not in standard?",
-        value: "What is included in my Northwind Health Plus plan that is not in standard?"
+        text: "Qual o EBITIDA do B2B no 4t21?",
+        value: "Qual o EBITIDA do B2B no 4t21?"
     },
-    { text: "What happens in a performance review?", value: "What happens in a performance review?" },
-    { text: "What does a Product Manager do?", value: "What does a Product Manager do?" }
+    { text: "Qual foi o resultado da BR Mania em 2022?", value: "Qual foi o resultado da BR Mania em 2022?" },
+    { text: "Qual o volume de vendas no 2t22?", value: "Qual o volume de vendas no 2t22?" }
 ];
 
 interface Props {
